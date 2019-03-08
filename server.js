bb1719e808dfaac406ab08b840f317860dd6004d
mahrliox <mramirez@anivia.mx>
2019-03-08 12:09:31 -0600

    Cambios para ANNY

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/open-app'));
+app.use(express.static(__dirname + '/dist/anny-devv'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/open-app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/anny-devv/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
